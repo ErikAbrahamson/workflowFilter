d9ebf53d265ff3eeaba159a3d5f1ee91b19d48cc
ErikAbrahamson <e.a.abrahamson@gmail.com>
2016-03-16 17:42:23 -0600

    update logic and filter dependencies

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
-prism.on('dashboardloaded', function() {
+prism.on('dashboardloaded', function(dimension) {
 
-    var filterArray;
-    if (prism.activeDashboard) {
-        prism.activeDashboard.filters.$$items.forEach(function(i) {
-            if (i.hasOwnProperty('levels') && i.levels[0].column === 'position_type') {
-                filterArray = i.levels;
-                position = filterArray[0].filter.members[0];
-                workflow = filterArray[1].filter.members[0];
-                return;
-            }
+    var workflow, position,
+        dash = prism.activeDashboard, options = { save: true, refresh: true };
+
+    if (dash) {
+        dash.filters.$$items.forEach(function(i) {
+            if (i.hasOwnProperty('jaql') && i.jaql.column === 'position_type') position = i.jaql.filter;
+            if (i.hasOwnProperty('jaql') && i.jaql.column === 'workflow') workflow = i.jaql.filter;
+            else return;
         });
     }
 
-    var position = filterArray[0].column,
-        workflow = filterArray[1].column;
+    switch (dash.filters.$$items.length > 0) {
+        case position.members[0] === 'Student':
 
-    switch (filterArray.length > 0) {
-        case position === 'Student':
-            console.log(workflow);
-            // disable faculty and and staff posting workflow filters
+            dash.filters.update({
+                jaql: {
+                    dim: dimension,
+                    filter: { members: ['Student Posting Workflow'] },
+                    datatype: 'text'
+                }
+            }, options);
+
+            dash.on('filterschanged', function(dash, update, jaql) {
+                // update filters element to reflect changes
+            });
             break;
-        case position === 'Faculty':
-            console.log(workflow);
-            // disable student and staff posting workflow filters
+
+        case position.members[0] === 'Faculty':
+            dash.filters.update({
+                jaql: {
+                    dim: dimension,
+                    filter: { members: ['Faculty Posting Workflow'] },
+                    datatype: 'text'
+                }
+            }, options);
+
+            dash.on('filterschanged', function(dash, update, jaql) {
+                // update filters element to reflect changes
+            });
             break;
-        case position === 'Staff':
-            console.log(workflow);
-            // dsiable student and faculty posting workflow filters
+
+        case position.members[0] === 'Staff':
+            dash.filters.update({
+                jaql: {
+                    dim: dimension,
+                    filter: { members: ['Staff HR Workflow'] },
+                    datatype: 'text'
+                }
+            }, options);
+
+            dash.on('filterschanged', function(dash, update, jaql) {
+                // update filters element to reflect changes
+            });
+
             break;
+
         default:
-            console.log('no position specified');
             break;
-            // set a default position maybe?
     }
-
 });
